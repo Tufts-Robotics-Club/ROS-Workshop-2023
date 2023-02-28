63cb81c7697cecec96baaf438257e75cda60fd1e
EmmaB3 <emma.l.bethel@gmail.com>
2023-02-28 02:27:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# Tufts Robotics 2023 ROS Workshop Starter Code
+
+Example ROS node for basic movement control in TurtleSim :)
+
+## How to Run
+Assuming you've followed the setup instructions sent out prior to the meeting, you should be able to run this code as follows:
+
+First, you should know which shell you are using (bash, zsh, etc). If you don't, run
+```sh
+# set up environment
+conda activate robostackenv
+cd ~/catkin_ws
+source devel/setup.sh
+
+# run the ros master (server over which the nodes communicate)
+roscore
+```
+
+This may take a moment, but should give command line output similar to
+```bash
+Checking log directory for disk usage. This may take a while.
+Press Ctrl-C to interrupt
+
+started roslaunch server http://Emmas-MacBook-Pro.local:64025/
+ros_comm version 1.15.14
+
+
+SUMMARY
+========
+
+PARAMETERS
+ * /rosdistro: noetic
+ * /rosversion: 1.15.14
+```
+
+In a separate command line window (while ros master continues running in the old one), run
+```sh
+# set up environment
+conda activate robostackenv
+cd ~/catkin_ws
+source devel/setup.sh
+
+# run the turtlesim
+rosrun turtlesim turtlesim_node
+```
+This should open a pop-up window with an unmoving turtle in the middle of it.
+
+In a 3rd command line window, run
+```sh
+# set up environment
+conda activate robostackenv
+cd ~/catkin_ws
+source devel/setup.sh
+
+# run the starter code
+rosrun turtle_demo turtle_controller.py
+```
+
+This should start the turtle moving in a random direction, bouncing off walls as needed. Feel free to add whatever functionality you want! If you find yourself out of ideas, here are some potential ones:
+
+- Draw a square
+
+- Draw a spiral
+
+- Move to particular coordinates (randomly-generated, or given as command line input)
+
+- Add another turtle & make them bounce off each other
+
+- Add like 10 turtles and make them ALL bounce off each other like a really sadistic aquarium
+
+## Setup Instructions
+
+For this workshop, we will be running ROS in a conda environment using robostackenv. If you don't have anaconda installed on your computer, you should first do that [here](https://docs.conda.io/en/latest/miniconda.html).
+
+Once you have conda installed, run the following commands in the command line (probably Terminal on MacOS, Powershell on Windows, etc):
+
+```sh
+# installing all the dependencies into a conda environment (this one will take a while; when it prompts you for input, just put the letter y and hit enter)
+conda create -n robostackenv ros-noetic-desktop python=3.9 -c robostack-staging -c conda-forge --no-channel-priority --override-channels
+
+# activating the envornment you just created
+conda activate robostackenv
+
+# creating a file structure for your ROS stuff
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+```
+If those commands complete without throwing errors, your environment should be set up! If you now run, 
+```sh
+ls
+```
+you should see three directory names; `build`, `devel`, and `src`. The entire contents of this github repo should be in the `src` directory, inside a directory called `turtle_demo`. If you alredy have git installed on your computer, you can accomplish this simply by running
+```sh
+cd src
+git clone 
+```
---
+++ src/turtle_controller.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+
+# NAME: turtle_controller.py
+# PURPOSE: randomly moves the turtle around the screen
+# AUTHOR: Emma Bethel
+
+
+import rospy
+import numpy as np
+import math
+import random
+
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+COLLISION_RADIUS = 0.5
+SPEED = 5
+HORIZONTAL_BOUNDARIES = (0, 11)
+VERTICAL_BOUNDARIES = (0, 11)
+CENTER = (5.5, 5.5)
+
+class TurtleController:
+
+    def __init__(self):
+        self.current_coords = np.array(CENTER)
+        self.rate = rospy.Rate(10)
+
+
+        # initializing publisher for sending turtle velocity commands
+        self.vel_pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+
+        # initializing subscriber for receiving and storing turtle position messages
+        self.pose_sub = rospy.Subscriber('turtle1/pose', Pose, self.update_current_coords)
+
+        # setting random starting velocity
+        self.current_vel = self.pick_random_vel()
+
+        # loop forever (until node is shut down)
+        while not rospy.is_shutdown():
+            # detecting and handling wall collisions
+            self.handle_collisions()
+
+            # sending velocity command
+            msg = Twist()
+            msg.linear.x = self.current_vel[0]
+            msg.linear.y = self.current_vel[1]
+            self.vel_pub.publish(msg)
+
+            # wait 10 miliseconds
+            self.rate.sleep()
+
+
+    # PURPOSE: randomly generates a velocity vector for turtle
+    # PARAMETERS: N/A
+    # RETURNS: the vector, as numpy array
+    @staticmethod
+    def pick_random_vel():
+        x = random.uniform(-SPEED, SPEED)
+        y = random.choice([-1, 1]) * math.sqrt(math.pow(SPEED, 2) - math.pow(x, 2))
+
+        return np.array((x, y))
+    
+    # PURPOSE: parses and stores turtle coordinate messages
+    # PARAMETERS: msg - message containing the turtle's current coordinates
+    # RETURNS: N/A
+    def update_current_coords(self, msg):
+        self.current_coords = np.array((msg.x, msg.y))
+
+    # PURPOSE: detects collisions with walls and updates direction accordingly
+    # PARAMETERS: N/A
+    # RETURNS: N/A
+    def handle_collisions(self):
+        # if vertical collision, reverse vertical component of velocity
+        if self.hit_top() or self.hit_bottom():
+            self.current_vel[1] *= -1
+        # if horizontal collision, reverse horizontal component of velocity
+        if self.hit_left() or self.hit_right():
+            self.current_vel[0] *= -1
+
+
+    # PURPOSE: checks whether turtle has hit top of screen
+    # PARAMETERS: N/A
+    # RETURNS: true if collision has occurred, false otherwise
+    def hit_top(self) -> bool:
+        return self.current_coords[1] + COLLISION_RADIUS >= VERTICAL_BOUNDARIES[1] and SPEED * self.current_vel[1] > 0
+    
+    # PURPOSE: checks whether turtle has hit bottom of screen
+    # PARAMETERS: N/A
+    # RETURNS: true if collision has occurred, false otherwise
+    def hit_bottom(self) -> bool:
+        return self.current_coords[1] - COLLISION_RADIUS <= VERTICAL_BOUNDARIES[0] and SPEED * self.current_vel[1] < 0
+
+    # PURPOSE: checks whether turtle has hit left edge of screen
+    # PARAMETERS: N/A
+    # RETURNS: true if collision has occurred, false otherwise
+    def hit_left(self) -> bool:
+        return self.current_coords[0] - COLLISION_RADIUS <= HORIZONTAL_BOUNDARIES[0] and SPEED * self.current_vel[0] < 0
+
+    # PURPOSE: checks whether turtle has hit right edge of screen
+    # PARAMETERS: N/A
+    # RETURNS: true if collision has occurred, false otherwise
+    def hit_right(self) -> bool:
+        return self.current_coords[0] + COLLISION_RADIUS >= HORIZONTAL_BOUNDARIES[1] and SPEED * self.current_vel[0] > 0
+
+
+if __name__=="__main__":
+    rospy.init_node('turtle_controller')
+    
+    TurtleController()
